86c71c89f99e640355baa03b7b379276ce9d473a
David Weiner <david.l.weiner30030@gmail.com>
2019-09-22 00:03:18 -0400

    Initial Commit

---
+++ src/ServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+namespace PersonalityViews;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__ . '../resources/views', 'personality');
+    }
+}

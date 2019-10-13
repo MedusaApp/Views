521bcda2f1d604d1d18561dcf7d4f86ba1987a6f
David Weiner <david.l.weiner30030@gmail.com>
2019-10-13 02:24:38 -0400

    Change menu order

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,6 +38,12 @@
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            @if (Auth::user()->isAn('admin'))
+                                <span class="dropdown-item-text">Admin Menu</span>
+                                <hr>
+                                <a class="dropdown-item" href="{{ route('admin.pending') }}">{{ __('Approve Memberships') }}</a>
+                                <hr>
+                            @endif
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
@@ -47,12 +53,6 @@
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                 @csrf
                             </form>
-                            @if (Auth::user()->isAn('admin'))
-                                <a id="adminMenu" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" area-haspopup="true" aria-expanded="false" v-pre>Admin Menu <span class="caret"></span></a>
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="adminMenu">
-                                    <a class="dropdown-item" href="{{ route('admin.pending') }}">{{ __('Approve Memberships') }}</a>
-                                </div>
-                            @endif
                         </div>
                     </li>
                 @endguest
